8b69096dfebb18c4792c3d975dea22ae7ee4e329
Dhruv Jain <dhruvjain9029@gmail.com>
2020-08-12 00:40:23 +0530

    Comments added

--- task_3_covid_tracker/app.js
+++ task_3_covid_tracker/app.js
@@ -1,18 +1,18 @@
 let a=0
 let total_cases=document.querySelector("#total_cases")
 let total_deaths=document.querySelector("#total_deaths")
-let total_recovered=document.querySelector("#total_recovered")
+let total_recovered=document.querySelector("#total_recovered")//selecting variables for world stats
 let total_active_cases=document.querySelector("#active_cases")
 let total_cases_per_million=document.querySelector("#cases_per_million")
 fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
-		"x-rapidapi-key": "c5d58e6967msh3f4f3f0fbdaa628p13eb8djsn66d180fdca4d"
+		"x-rapidapi-key": "c5d58e6967msh3f4f3f0fbdaa628p13eb8djsn66d180fdca4d"//fetching
 	}
 })
 .then(response => {
-	return (response.json());
+	return (response.json());//recovering the json part of the all what was fetched
 })
 .then(data=>{
     console.log(data)
@@ -29,45 +29,45 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
     for(let i = 0; i<countries_stat.length;i++){
         
         
-		let row = table.insertRow(i+1);
+		let row = table.insertRow(i+1);//adding a row to table
 
         let country_name = row.insertCell(0);
         let cases = row.insertCell(1);
         let deaths = row.insertCell(2);
         let active_cases = row.insertCell(3);
-		let recovered_per_country = row.insertCell(4);
+		let recovered_per_country = row.insertCell(4);//adding cells in given column index 
 		let tested=row.insertCell(5);
 		let graph=row.insertCell(6)
 		let more=row.insertCell(7)
 		
         country_name.innerHTML = countries_stat[i].country_name;
         cases.innerHTML = countries_stat[i].cases;
-        deaths.innerHTML = countries_stat[i].deaths;
+        deaths.innerHTML = countries_stat[i].deaths;//filling the cells with appropriate data w.r.t column head
         active_cases.innerHTML = countries_stat[i].active_cases;
         recovered_per_country.innerHTML = countries_stat[i].total_recovered;
 		tested.innerHTML=countries_stat[i].total_tests;
-		more.innerHTML=`<button class='btn btn-primary' data-toggle="modal" data-target="#countryModal" id='${countries_stat[i].country_name}'>Know More</button>`
+		more.innerHTML=`<button class='btn btn-primary' data-toggle="modal" data-target="#countryModal" id='${countries_stat[i].country_name}'>Know More</button>`//adding buttons in the required cell and setting them up with respective modal to come
 		graph.innerHTML=`<button class='btn btn-success' data-toggle="modal" data-target="#graphModal" id='${countries_stat[i].country_name}'>View Graph</button>`
 		
 
 		
 	}
-	document.addEventListener('click',function(e){
+	document.addEventListener('click',function(e){ //adding the click event on the button by finding it
 		if(e.target.className==='btn btn-success'){
 			
 			let arr=undefined
-			var nation=e.target.id
+			var nation=e.target.id      //getting to know which country's button has been clicked
 			countries_stat.forEach(function(c){
-				if(c.country_name===nation){
+				if(c.country_name===nation){  
 					
-					arr=c;
+					arr=c; //getting all stats of that country
 				}
 			
 			})
-			
+			//setting up content for modal of graph
 			const modalGraphTitle=document.querySelector('#graph-title')
 			var a=(arr.active_cases)
-			a=a.replace(/\,/g,'')
+			a=a.replace(/\,/g,'')//removing commas from the value and converting it to int
 			a=parseInt(a)
 			var b=arr.deaths
 			b=b.replace(/\,/g,'')
@@ -77,7 +77,7 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
 			c=parseInt(c)
 			modalGraphTitle.innerHTML=`${arr.country_name} Covid-Graph`
 			const modalGraphBody=document.querySelector('#graph-body')
-			modalGraphBody.innerHTML=`<canvas id="myChart"></canvas>`
+			modalGraphBody.innerHTML=`<canvas id="myChart"></canvas>`//setting up canvas for chart js
 			var pieChart=document.querySelector('#myChart').getContext('2d');
 			var covidChart=new Chart(pieChart,{
 				type:'pie',
@@ -101,13 +101,13 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
 			let cou=undefined
 			var country=e.target.id
 			countries_stat.forEach(function(c){
-				if(c.country_name===country){
+				if(c.country_name===country){ 
 					
 					cou=c;
 				}
 			
 			})
-			
+			//setting up the other modal
 			const mtitle=document.querySelector('.modal-title')
 			mtitle.innerHTML=`${cou.country_name} Covid-Data`
 			const mbody=document.querySelector('.modal-body')
@@ -126,16 +126,16 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
 		}
 	})
 	
-	
+	//search functionality
 	searchInput.addEventListener("keyup",function(){
-		var filter=searchInput.value.toUpperCase()
+		var filter=searchInput.value.toUpperCase()  //we convert everything to uppercase so that input can be case insensitive
 		for(let i=0; i<tr.length;i++){
-			td=tr[i].getElementsByTagName("td")[0]
+			td=tr[i].getElementsByTagName("td")[0] //getting td of the 0th column of the ith tr in the nodelist
 			if(td){
 				
 				let txtValue=td.textContent
-				if(txtValue.toUpperCase().indexOf(filter) > -1){
-					tr[i].style.display=""
+				if(txtValue.toUpperCase().indexOf(filter) > -1){ //checks if the phrase entered is present anywhere inside 
+					tr[i].style.display="" 
 				}else{
 					tr[i].style.display="none"
 				}
